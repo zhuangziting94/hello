7cf8a7d7dc184d6b7b9d6e24e886e7ff9fc29d84
zhuangziting94 <zziting94@gmail.com>
2024-11-12 14:50:01 +0800

    Create hello_test.go

---
+++ hello_test.go
@@ -0,0 +1,19 @@
+package hello_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com:zhuangziting94/hello"
+)
+
+func TestHello(t *testing.T) {
+	data := "jack"
+	expected := fmt.Sprintf("hello %s!", data)
+	result := hello.Hello(data)
+
+	if result != expected {
+		t.Fatalf("expected result %s, but got %s", expected, result)
+	}
+
+}
